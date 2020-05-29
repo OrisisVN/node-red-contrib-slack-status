d830becc1423c72896d8d1014bbf3e8d4daa4897
huy.quangvu <huy.quangvu@mor.com.vn>
2020-05-29 10:55:43 +0700

    Send dynamic value success

--- source/slackStatus.js
+++ source/slackStatus.js
@@ -6,24 +6,22 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, n);
         var node = this;
 
-        var newObj = n.data || {}
+        var flowObj = n.data || {}
         var request = require('request');
-
-        var methodValue = n.method
+        var methodValue = n.emoji
         var childPathProperty = n.childpath || ""
         var propertyType = n.propertyType || "msg";
         var globalContext = this.context().global;
         var flowContext = this.context().flow;
 
         var slackCertificate = RED.nodes.getNode(n.slackCertificate);
-        var url_params = "";
 
         node.on("input", function (msg) {
             node.status({});
 
             // console.log("methodValue", methodValue);
+            // console.log("childPathProperty ", childPathProperty)
             // console.log("propertyType", propertyType);
-            // console.log("childPathProperty", childPathProperty);
 
             // select childPath
             var childPath = "";
@@ -46,37 +44,33 @@ module.exports = function (RED) {
             }
             if (methodValue == "setPriority" || methodValue == "setWithPriority") {
                 methodValue = "put"
-            } else if (methodValue == "msg.method" || methodValue == "") {
-                methodValue = msg.method
+            } else if (methodValue == "msg.emoj" || methodValue == "") {
+                methodValue = msg.emoji
             };
+            requestData(flowObj, methodValue, node, msg, slackCertificate, childPathProperty)
         })
-        // var testObj = {
-        //     "profile": {
-        //         "status_text": "Traveling",
-        //         "status_emoji": ":palm_tree:",
-        //         "status_expiration": 0
-        //     }
-        // };
-        // request({
-        //     url: "https://slack.com/api/users.profile.set",
-        //     method: "POST",
-        //     json: true,
-        //     headers: {
-        //         "Content-type": "application/json; charset=utf-8",
-        //         "Authorization": "Bearer " + slackCertificate['accesstoken'],
-        //     },
-        //     body: testObj
-        // }, function (error, response, body) {
-        //     console.log(response);
-        // });
+
     }
-    function requestData(newObj, methodValue, url_params, url_params_childpath, node, msg) {
-        //     var opts = {
-        //         method: 'POST',
-        //         url: 'https://slack.com/api/users.profile.set',
-        //         body: JSON.stringify(newObj)
-        //     }
-        console.log(slackCertificate['accesstoken'])
+    function requestData(flowObj, methodValue, node, msg, slackCertificate, childPathProperty) {
+        var flowObj = {
+            "profile": {
+                "status_text": childPathProperty,
+                "status_emoji": methodValue,
+                "status_expiration": 0
+            }
+        };
+        request({
+            url: "https://slack.com/api/users.profile.set",
+            method: "POST",
+            json: true,
+            headers: {
+                "Content-type": "application/json; charset=utf-8",
+                "Authorization": "Bearer " + slackCertificate['accesstoken'],
+            },
+            body: flowObj
+        }, function (error, response, body) {
+            console.log(response);
+        });
     }
 
     RED.nodes.registerType("slackStatus", slackStatus);
